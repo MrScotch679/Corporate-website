4fbb6e19c96e5cda32ba15c8a04a2427af9ef9ee
Vlad <vladis67948@gmail.com>
2022-07-21 11:40:05 +0300

    demo

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const optimization = () => {
 
   if (isProd) {
     config.minimizer = [
-      new CssMinimizerPlugin(),
+      // new CssMinimizerPlugin(),
       new TerserPlugin(),
     ];
   }
@@ -50,7 +50,7 @@ module.exports = {
   },
   output: {
     filename: filename('js'),
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'docs')
   },
   optimization: optimization(),
   devServer: {
